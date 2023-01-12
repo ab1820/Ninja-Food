9ad36103a2156aebebcfef535183969b2534eea9
Amanda Buttineau <114027684+ab1820@users.noreply.github.com>
2023-01-12 05:17:03 -0500

    initial setup
    first time with tailwind css, initial commit with starter files - can begin to style now

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const { join } = require('path');
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
